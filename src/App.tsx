ac638f099be1d095235a4e40baec6b11e4a22285
abtinzero <abtinsaadat@gmail.com>
2023-05-17 17:21:39 +0200

    Pass children

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 function App() {
-  let items = ["New York", "San Francisco"];
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectedItem}
-      ></ListGroup>
+      <Alert>Hello World</Alert>
     </div>
   );
 }
-
 export default App;
